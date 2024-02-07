c7af0e6505a051f864940b4832ab81ee6a32c3bb
Aayush Bharti <aayushbh2002@gmail.com>
2024-02-07 19:52:35 +0530

    update

---
+++ script.js
@@ -0,0 +1,14 @@
+const modal = document.querySelector(".modal");
+const overlay = document.querySelector(".overlay");
+
+//   Modal open function
+const openModal = () => {
+  modal.classList.add("active");
+  overlay.classList.add("overlayactive");
+};
+
+// Modal close function
+const closeModal = () => {
+  modal.classList.remove("active");
+  overlay.classList.remove("overlayactive");
+};
