8fc604c02edba5f3433b6c04d619ed62f621cbd0
Aayush Bharti <aayushbh2002@gmail.com>
2024-02-07 23:15:58 +0530

    update

--- script.js
+++ script.js
@@ -2,13 +2,14 @@ const modal = document.querySelector(".modal");
 const overlay = document.querySelector(".overlay");
 
 //   Modal open function
-const openModal = () => {
+function openModal(){
   modal.classList.add("active");
   overlay.classList.add("overlayactive");
-};
+}
 
 // Modal close function
-const closeModal = () => {
+// another way- an anonymous function is assigined to closeModal  
+const closeModal = () => {    
   modal.classList.remove("active");
   overlay.classList.remove("overlayactive");
 };
